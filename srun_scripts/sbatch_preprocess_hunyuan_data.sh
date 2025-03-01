1f09527ae6698262a7c8048b1a69302a83f2bbd4
Xinrui014 <WANG1791@e.ntu.edu.sg>
2025-03-01 16:11:06 +0800

    Update sbatch_preprocess_hunyuan_data.sh

--- srun_scripts/sbatch_preprocess_hunyuan_data.sh
+++ srun_scripts/sbatch_preprocess_hunyuan_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=lanqing001
 #SBATCH -A CGAI24022
-#SBATCH --nodes=4               # Request 4 nodes
+#SBATCH --nodes=8               # Request 4 nodes
 #SBATCH --ntasks-per-node=1     # Ensure 1 task per node
-#SBATCH --time=30:00:00         # Max runtime
+#SBATCH --time=10:00:00         # Max runtime
 #SBATCH --partition=gh        # GPU partition (or adjust based on your system)
 #SBATCH --error=/scratch/10320/lanqing001/xinrui/FastVideo/srun_scripts/output/job.%J.err
 #SBATCH --output=/scratch/10320/lanqing001/xinrui/FastVideo/srun_scripts/output/job.%J.out
@@ -24,15 +24,15 @@ cd /scratch/10320/lanqing001/xinrui/FastVideo
 export MASTER_ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
 export MASTER_PORT=12355
 
-srun -N 4 -n 4 \
-    python /scratch/10320/lanqing001/xinrui/FastVideo/data_preprocess/preprocess_vae_latents.py \
+srun -N 8 -n 8 \
+    python /scratch/10320/lanqing001/xinrui/FastVideo/fastvideo/data_preprocess/preprocess_vae_latents.py \
     --model_path "/scratch/10320/lanqing001/xinrui/FastVideo/data/hunyuan" \
     --data_merge_path "/scratch/10320/lanqing001/xinrui/FastVideo/data/raw_mp4_10k_720/merge.txt" \
     --train_batch_size=8 \
     --max_height=720 \
     --max_width=1080 \
     --num_frames=45 \
-    --dataloader_num_workers 1 \
+    --dataloader_num_workers 32 \
     --output_dir="/scratch/10320/lanqing001/xinrui/FastVideo/data/raw_mp4_10k_1088/" \
     --model_type "hunyuan" \
     --train_fps 8
